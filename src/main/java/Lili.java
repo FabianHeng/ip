c47301f732462d5652ddc28d005a6aa12b9a7343
FabianHeng <fabianheng@gmail.com>
2025-01-22 13:19:49 +0800

    Added marking and unmarking in list

--- src/main/java/Lili.java
+++ src/main/java/Lili.java
@@ -20,33 +20,55 @@ public class Lili {
 
         Scanner scanner = new Scanner(System.in);
         String input;
-        ArrayList<String> list = new ArrayList<>();
+        ArrayList<Task> list = new ArrayList<>();
         while (true) {
             input = scanner.nextLine();
 
-            if (input.equalsIgnoreCase("bye")) {
+            if (input.equalsIgnoreCase("bye") || input.equalsIgnoreCase("exit") || input.equalsIgnoreCase("quit")) {
                 System.out.println("------------------------------");
-                System.out.println("Byee, talk to you again <3");
+                System.out.println("Bye, talk to you again <3");
                 System.out.println("------------------------------");
                 break;
             }
 
-            if (input.equalsIgnoreCase("list")) {
-                System.out.println("------------------------------");
+            System.out.println("------------------------------");
+            if (input.equalsIgnoreCase("list") || input.equalsIgnoreCase("ls")) {
                 if (list.isEmpty()) {
                     System.out.println("Nothing in list");
                 } else {
+                    System.out.println("Here are your list of tasks:");
                     for (int i = 0; i < list.size(); i++) {
-                        System.out.println((i + 1) + ". " + list.get(i));
+                        System.out.println((i + 1) + ". " + list.get(i).toString());
                     }
                 }
-                System.out.println("------------------------------");
-            } else {
-                System.out.println("------------------------------");
-                list.add(input);
-                System.out.println("Added: " + input);
-                System.out.println("------------------------------");
+            } else if (input.toLowerCase().startsWith("mark ")) {
+                int taskNumber = Integer.parseInt(input.split(" ")[1]);
+                if (taskNumber <= 0 || taskNumber > list.size()) {
+                    System.out.println("I can't find any such task, please try again >.<");
+                } else {
+                    Task task = list.get(taskNumber - 1);
+                    task.markAsDone();
+                    System.out.println("Yay! I've marked it as done:");
+                    System.out.println(task.toString());
+                }
+            } else if (input.toLowerCase().startsWith("unmark ")) {
+                int taskNumber = Integer.parseInt(input.split(" ")[1]);
+                if (taskNumber <= 0 || taskNumber > list.size()) {
+                    System.out.println("I can't find any such task, please try again >.<");
+                } else {
+                    Task task = list.get(taskNumber - 1);
+                    task.markAsNotDone();
+                    System.out.println("Ok! I've marked it as not done yet:");
+                    System.out.println(task.toString());
+                }
             }
+            else {
+                Task task = new Task(input);
+                list.add(task);
+                System.out.println("Nice! I've added it to your list:");
+                System.out.println(task.toString());
+            }
+            System.out.println("------------------------------");
         }
 
         scanner.close();
