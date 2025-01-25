76e45e019fe0065353de2363d219fbe97da38d71
FabianHeng <fabianheng@gmail.com>
2025-01-25 22:36:16 +0800

    Add MarkCommand class

---
+++ src/main/java/MarkCommand.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+
+public class MarkCommand extends Command {
+    private final String taskNumber;
+
+    public MarkCommand(String taskNumber) {
+        this.taskNumber = taskNumber;
+    }
+
+    /**
+     * Marks a task as done.
+     *
+     * @param taskList The list of tasks.
+     * @param ui The user interface that prints out messages.
+     * @param storage The loading and saving files object.
+     * @throws InvalidTaskNumberException If the task number is invalid.
+     */
+    @Override
+    public void execute(ArrayList<Task> taskList, Ui ui, Storage storage) throws LiliException {
+        int taskIndex = parseTaskNumber(taskNumber, taskList.size());
+        Task task = taskList.get(taskIndex);
+        task.markAsDone();
+        ui.printChatText("MARK");
+        System.out.println(task);
+    }
+
+    private int parseTaskNumber(String taskNumber, int size) throws InvalidTaskNumberException {
+        try {
+            int index = Integer.parseInt(taskNumber) - 1;
+            if (index < 0 || index >= size) {
+                throw new InvalidTaskNumberException();
+            }
+            return index;
+        } catch (NumberFormatException e) {
+            throw new InvalidTaskNumberException();
+        }
+    }
+}
