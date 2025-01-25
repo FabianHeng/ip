4441149c2ec4d41e3f939e15786a177c8bb2b4ba
FabianHeng <fabianheng@gmail.com>
2025-01-25 22:37:22 +0800

    Lili class: Update class to match OOP design

--- src/main/java/Lili.java
+++ src/main/java/Lili.java
@@ -12,19 +12,6 @@ import java.util.Scanner;
  */
 
 public class Lili {
-    /**
-     * Enum for supported commands.
-     */
-    private enum Command {
-        LIST,
-        LS,
-        MARK,
-        UNMARK,
-        TODO,
-        DEADLINE,
-        EVENT,
-        DELETE
-    }
 
     private static final ArrayList<Task> taskList = new ArrayList<>();
 
@@ -75,182 +62,114 @@ public class Lili {
      */
     private static void handleCommand(String input) throws LiliException {
         String[] parts = input.split(" ", 2);
-        Command command;
+        String commandWord = parts[0].toUpperCase();
+        String argument = parts.length > 1 ? parts[1].trim() : "";
 
         try {
-            command = Command.valueOf(parts[0].toUpperCase());
+            CommandType commandType = CommandType.fromString(commandWord);
+            Command command = commandType.createCommand(argument);
+            command.execute(taskList, ui, storage);
         } catch (IllegalArgumentException e) {
             throw new InvalidCommandException();
         }
-
-        String argument = parts.length > 1 ? parts[1].trim() : "";
-
-        switch (command) {
-        case LIST:
-        case LS:
-            displayTaskList();
-            break;
-        case MARK:
-            markTask(argument);
-            break;
-        case UNMARK:
-            unmarkTask(argument);
-            break;
-        case TODO:
-            addTodoTask(argument);
-            break;
-        case DEADLINE:
-            addDeadlineTask(argument);
-            break;
-        case EVENT:
-            addEventTask(argument);
-            break;
-        case DELETE:
-            deleteTask(argument);
-            break;
-        default:
-            throw new InvalidCommandException();
-        }
     }
 
-    /**
-     * Displays the list of tasks.
-     */
-    private static void displayTaskList() {
-        if (taskList.isEmpty()) {
-            ui.printChatText("LIST_EMPTY");
-        } else {
-            ui.printChatText("LIST");
-            for (int i = 0; i < taskList.size(); i++) {
-                System.out.println((i + 1) + ". " + taskList.get(i).toString());
-            }
-        }
-    }
-
-    /**
-     * Marks a task as done.
-     *
-     * @param taskNumber Task number to mark.
-     * @throws InvalidTaskNumberException If the task number is invalid.
-     */
-    private static void markTask(String taskNumber) throws InvalidTaskNumberException {
-        int taskIndex = parseTaskNumber(taskNumber);
-        Task task = taskList.get(taskIndex);
-        task.markAsDone();
-        ui.printChatText("MARK");
-        System.out.println(task.toString());
-    }
-
-    /**
-     * Unmarks a task as not done.
-     *
-     * @param taskNumber Task number to unmark.
-     * @throws InvalidTaskNumberException If the task number is invalid.
-     */
-    private static void unmarkTask(String taskNumber) throws InvalidTaskNumberException {
-        int taskIndex = parseTaskNumber(taskNumber);
-        Task task = taskList.get(taskIndex);
-        task.markAsNotDone();
-        ui.printChatText("UNMARK");
-        System.out.println(task.toString());
-    }
-
-    /**
-     * Adds a new Todo task.
-     *
-     * @param description Description of the task.
-     * @throws InvalidTodoDescriptionException If the description is empty.
-     */
-    private static void addTodoTask(String description) throws InvalidTodoDescriptionException {
-        if (description.isEmpty()) {
-            throw new InvalidTodoDescriptionException();
-        }
-        Todo todo = new Todo(description);
-        taskList.add(todo);
-        displayTaskAdded(todo);
-    }
-
-    /**
-     * Adds a new Deadline task.
-     *
-     * @param description Task description and deadline, separated by " /by ".
-     * @throws InvalidDeadlineFormatException If the command format is invalid.
-     */
-    private static void addDeadlineTask(String description) throws InvalidDeadlineFormatException {
-        String[] parts = description.split(" /by ");
-        if (parts.length != 2) {
-            throw new InvalidDeadlineFormatException();
-        }
-        try {
-            Deadline deadline = new Deadline(parts[0].trim(), parts[1].trim());
-            taskList.add(deadline);
-            displayTaskAdded(deadline);
-        } catch (DateTimeParseException e) {
-            System.out.println("I can't understand the date given, make sure it is in this format (default time is 0000): yyyy-mm-dd HHmm.");
-        }
-    }
-
-    /**
-     * Adds a new Event task.
-     *
-     * @param description Task description with start and end time, separated by " /from " and " /to ".
-     * @throws InvalidEventFormatException If the format is invalid.
-     */
-    private static void addEventTask(String description) throws InvalidEventFormatException {
-        String[] parts = description.split(" /from | /to ");
-        if (parts.length != 3) {
-            throw new InvalidEventFormatException();
-        }
-        try {
-            Event event = new Event(parts[0].trim(), parts[1].trim(), parts[2].trim());
-            taskList.add(event);
-            displayTaskAdded(event);
-        } catch (DateTimeParseException e) {
-            System.out.println("I can't understand the date(s) given, make sure it is in this format (default time is 0000): yyyy-mm-dd HHmm.");
-        }
-    }
-
-    /**
-     * Deletes a task.
-     *
-     * @param taskNumber Task number to delete.
-     * @throws InvalidTaskNumberException If the task number is invalid.
-     */
-    private static void deleteTask(String taskNumber) throws InvalidTaskNumberException {
-        int taskIndex = parseTaskNumber(taskNumber);
-        Task task = taskList.remove(taskIndex);
-        ui.printChatText("DELETE");
-        System.out.println(task.toString());
-        System.out.println("Now you have " + taskList.size() + " task(s) in your list.");
-    }
-
-    /**
-     * Displays a message for a newly added task.
-     *
-     * @param task The task that was added.
-     */
-    private static void displayTaskAdded(Task task) {
-        ui.printChatText("TASK");
-        System.out.println(task.toString());
-        System.out.println("Now you have " + taskList.size() + " task(s) in your list.");
-    }
-
-    /**
-     * Parses a task number from user input.
-     *
-     * @param taskNumber The task number as a string.
-     * @return The zero-based index of the task.
-     * @throws InvalidTaskNumberException If the task number is invalid.
-     */
-    private static int parseTaskNumber(String taskNumber) throws InvalidTaskNumberException {
-        try {
-            int taskIndex = Integer.parseInt(taskNumber) - 1;
-            if (taskIndex < 0 || taskIndex >= taskList.size()) {
-                throw new InvalidTaskNumberException();
-            }
-            return taskIndex;
-        } catch (NumberFormatException e) {
-            throw new InvalidTaskNumberException();
-        }
-    }
+//    /**
+//     * Adds a new Todo task.
+//     *
+//     * @param description Description of the task.
+//     * @throws InvalidTodoDescriptionException If the description is empty.
+//     */
+//    private static void addTodoTask(String description) throws InvalidTodoDescriptionException {
+//        if (description.isEmpty()) {
+//            throw new InvalidTodoDescriptionException();
+//        }
+//        Todo todo = new Todo(description);
+//        taskList.add(todo);
+//        displayTaskAdded(todo);
+//    }
+//
+//    /**
+//     * Adds a new Deadline task.
+//     *
+//     * @param description Task description and deadline, separated by " /by ".
+//     * @throws InvalidDeadlineFormatException If the command format is invalid.
+//     */
+//    private static void addDeadlineTask(String description) throws InvalidDeadlineFormatException {
+//        String[] parts = description.split(" /by ");
+//        if (parts.length != 2) {
+//            throw new InvalidDeadlineFormatException();
+//        }
+//        try {
+//            Deadline deadline = new Deadline(parts[0].trim(), parts[1].trim());
+//            taskList.add(deadline);
+//            displayTaskAdded(deadline);
+//        } catch (DateTimeParseException e) {
+//            System.out.println("I can't understand the date given, make sure it is in this format (default time is 0000): yyyy-mm-dd HHmm.");
+//        }
+//    }
+//
+//    /**
+//     * Adds a new Event task.
+//     *
+//     * @param description Task description with start and end time, separated by " /from " and " /to ".
+//     * @throws InvalidEventFormatException If the format is invalid.
+//     */
+//    private static void addEventTask(String description) throws InvalidEventFormatException {
+//        String[] parts = description.split(" /from | /to ");
+//        if (parts.length != 3) {
+//            throw new InvalidEventFormatException();
+//        }
+//        try {
+//            Event event = new Event(parts[0].trim(), parts[1].trim(), parts[2].trim());
+//            taskList.add(event);
+//            displayTaskAdded(event);
+//        } catch (DateTimeParseException e) {
+//            System.out.println("I can't understand the date(s) given, make sure it is in this format (default time is 0000): yyyy-mm-dd HHmm.");
+//        }
+//    }
+//
+//    /**
+//     * Deletes a task.
+//     *
+//     * @param taskNumber Task number to delete.
+//     * @throws InvalidTaskNumberException If the task number is invalid.
+//     */
+//    private static void deleteTask(String taskNumber) throws InvalidTaskNumberException {
+//        int taskIndex = parseTaskNumber(taskNumber);
+//        Task task = taskList.remove(taskIndex);
+//        ui.printChatText("DELETE");
+//        System.out.println(task.toString());
+//        System.out.println("Now you have " + taskList.size() + " task(s) in your list.");
+//    }
+//
+//    /**
+//     * Displays a message for a newly added task.
+//     *
+//     * @param task The task that was added.
+//     */
+//    private static void displayTaskAdded(Task task) {
+//        ui.printChatText("TASK");
+//        System.out.println(task.toString());
+//        System.out.println("Now you have " + taskList.size() + " task(s) in your list.");
+//    }
+//
+//    /**
+//     * Parses a task number from user input.
+//     *
+//     * @param taskNumber The task number as a string.
+//     * @return The zero-based index of the task.
+//     * @throws InvalidTaskNumberException If the task number is invalid.
+//     */
+//    private static int parseTaskNumber(String taskNumber) throws InvalidTaskNumberException {
+//        try {
+//            int taskIndex = Integer.parseInt(taskNumber) - 1;
+//            if (taskIndex < 0 || taskIndex >= taskList.size()) {
+//                throw new InvalidTaskNumberException();
+//            }
+//            return taskIndex;
+//        } catch (NumberFormatException e) {
+//            throw new InvalidTaskNumberException();
+//        }
+//    }
 }
