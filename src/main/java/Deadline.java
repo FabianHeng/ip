4c9d0cc5dd442dcbba1a9eabd021bc77cdb00128
FabianHeng <fabianheng@gmail.com>
2025-01-25 12:09:06 +0800

    Deadline class: Add date formatter

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,23 +1,33 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
 public class Deadline extends Task {
-    protected String by;
+    protected LocalDateTime dateTime;
+    protected DateTimeFormatter displayFormatter = DateTimeFormatter.ofPattern("MMM dd yyyy");
 
     public Deadline(String name, String by) {
         super(name);
-        this.by = by;
+        this.dateTime = parseDateTime(by);
     }
 
     public Deadline(String name, String by, boolean isDone) {
         super(name, isDone);
-        this.by = by;
+        this.dateTime = parseDateTime(by);
+    }
+
+    public LocalDateTime parseDateTime(String by) throws DateTimeParseException {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        return LocalDateTime.parse(by, formatter);
     }
 
     @Override
     public String toFileFormat() {
-        return "D | " + (isDone ? "1" : "0") + " | " + name + " | " + by;
+        return "D | " + (isDone ? "1" : "0") + " | " + name + " | " + dateTime.format(displayFormatter);
     }
 
     @Override
     public String toString() {
-        return "[D] " + super.toString() + " (by: " + by + ")";
+        return "[D] " + super.toString() + " (by: " + dateTime.format(displayFormatter) + ")";
     }
 }
