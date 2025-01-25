cfd1996f69106d3dd3e48c173cd8c72264f25f67
FabianHeng <fabianheng@gmail.com>
2025-01-25 22:36:06 +0800

    Add ListCommand class

---
+++ src/main/java/ListCommand.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+
+public class ListCommand extends Command {
+    /**
+     * Displays the list of tasks.
+     *
+     * @param taskList The list of tasks.
+     * @param ui The user interface that prints out messages.
+     * @param storage The loading and saving files object.
+     */
+    @Override
+    public void execute(ArrayList<Task> taskList, Ui ui, Storage storage) {
+        if (taskList.isEmpty()) {
+            ui.printChatText("LIST_EMPTY");
+        } else {
+            ui.printChatText("LIST");
+            for (int i = 0; i < taskList.size(); i++) {
+                System.out.println((i + 1) + ". " + taskList.get(i).toString());
+            }
+        }
+    }
+}
