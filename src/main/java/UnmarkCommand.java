512f5e36ec9e36b181bc51d253857a9cd4ca7ca1
FabianHeng <fabianheng@gmail.com>
2025-01-25 22:36:42 +0800

    Add UnmarkCommand class

---
+++ src/main/java/UnmarkCommand.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+
+public class UnmarkCommand extends Command {
+    private final String taskNumber;
+
+    public UnmarkCommand(String taskNumber) {
+        this.taskNumber = taskNumber;
+    }
+
+    /**
+     * Unmarks a task as not done.
+     *
+     * @param taskList The list of tasks.
+     * @param ui The user interface that prints out messages.
+     * @param storage The loading and saving files object.
+     * @throws InvalidTaskNumberException If the task number is invalid.
+     */
+    @Override
+    public void execute(ArrayList<Task> taskList, Ui ui, Storage storage) throws LiliException {
+        int taskIndex = parseTaskNumber(taskNumber, taskList.size());
+        Task task = taskList.get(taskIndex);
+        task.markAsNotDone();
+        ui.printChatText("UNMARK");
+        System.out.println(task);
+    }
+
+    private int parseTaskNumber(String taskNumber, int size) throws InvalidTaskNumberException {
+        try {
+            int index = Integer.parseInt(taskNumber) - 1;
+            if (index < 0 || index >= size) {
+                throw new InvalidTaskNumberException();
+            }
+            return index;
+        } catch (NumberFormatException e) {
+            throw new InvalidTaskNumberException();
+        }
+    }
+}
