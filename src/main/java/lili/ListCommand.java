650ebd166e2cc5a325a7b7cda8feb39299ee3876
FabianHeng <fabianheng@gmail.com>
2025-02-10 11:46:35 +0800

    Add assertions to ListCommand class

--- src/main/java/lili/ListCommand.java
+++ src/main/java/lili/ListCommand.java
@@ -12,9 +12,14 @@ public class ListCommand extends Command {
      * @param taskList The list of tasks.
      * @param ui The user interface that prints out messages.
      * @param storage The loading and saving files object.
+     * @return The response message of Lilibot.
      */
     @Override
     public String execute(ArrayList<Task> taskList, Ui ui, Storage storage) {
+        assert taskList != null : "Task list should not be null";
+        assert ui != null : "Ui object should not be null";
+        assert storage != null : "Storage object should not be null";
+
         if (taskList.isEmpty()) {
             return "LIST_EMPTY\nNo tasks available.";
         } else {
@@ -22,6 +27,7 @@ public class ListCommand extends Command {
             for (int i = 0; i < taskList.size(); i++) {
                 response.append((i + 1)).append(". ").append(taskList.get(i).toString()).append("\n");
             }
+            assert response.toString().startsWith("LIST\n") : "Response should start with 'LIST'";
             return response.toString();
         }
     }
