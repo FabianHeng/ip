9259d4d02b4bee6d9460a9279e9c8306fe4d79a0
FabianHeng <fabianheng@gmail.com>
2025-02-16 00:24:51 +0800

    Add title and app icon

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,7 @@ import java.io.IOException;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 import lili.Lili;
@@ -25,10 +26,14 @@ public class Main extends Application {
         try {
             FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/view/MainWindow.fxml"));
             AnchorPane ap = fxmlLoader.load();
+
             Scene scene = new Scene(ap);
             stage.setMinHeight(600);
             stage.setMinWidth(420);
+            stage.getIcons().add(new Image(this.getClass().getResourceAsStream("/images/icon.png")));
+            stage.setTitle("LiliBot");
             stage.setScene(scene);
+
             fxmlLoader.<MainWindow>getController().setLili(lili);
             stage.show();
         } catch (IOException e) {
